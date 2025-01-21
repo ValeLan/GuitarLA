3012cc9c7e79acaad901180e6e61dbe5db457d4d
ValeLan <vale.lanf@gmail.com>
2025-01-21 17:20:40 -0300

    Maneje estados para hacer el carrito dinamico

--- src/Components/Header.jsx
+++
@@ -1,6 +0,0 @@
-export default function Header() {
-    return(
-        <h1>Desde Header</h1>
-    )
-}
-
---
+++ src/data/db.js
@@ -0,0 +1,86 @@
+export const db = [
+    {
+      id: 1,
+      name: "Guitarra Acústica Clásica",
+      image: "guitarra_01",
+      description: "Guitarra clásica para principiantes.",
+      price: 150
+    },
+    {
+      id: 2,
+      name: "Guitarra Eléctrica Vintage",
+      image: "guitarra_02",
+      description: "Modelo clásico de sonido auténtico.",
+      price: 300
+    },
+    {
+      id: 3,
+      name: "Guitarra Electroacústica",
+      image: "guitarra_03",
+      description: "Versátil para escenarios y estudio.",
+      price: 400
+    },
+    {
+      id: 4,
+      name: "Guitarra Bajo Profesional",
+      image: "guitarra_04",
+      description: "Ideal para músicos avanzados.",
+      price: 600
+    },
+    {
+      id: 5,
+      name: "Guitarra Infantil",
+      image: "guitarra_05",
+      description: "Perfecta para niños que inician.",
+      price: 120
+    },
+    {
+      id: 6,
+      name: "Guitarra Flamenca",
+      image: "guitarra_06",
+      description: "Especial para el estilo flamenco.",
+      price: 350
+    },
+    {
+      id: 7,
+      name: "Guitarra de Jazz",
+      image: "guitarra_07",
+      description: "Tono cálido para amantes del jazz.",
+      price: 500
+    },
+    {
+      id: 8,
+      name: "Guitarra de Viaje",
+      image: "guitarra_08",
+      description: "Compacta y ligera para viajar.",
+      price: 200
+    },
+    {
+      id: 9,
+      name: "Guitarra Premium de Colección",
+      image: "guitarra_09",
+      description: "Edición limitada para coleccionistas.",
+      price: 1000
+    },
+    {
+      id: 10,
+      name: "Guitarra Semi-Hueca",
+      image: "guitarra_10",
+      description: "Ideal para géneros como blues.",
+      price: 450
+    },
+    {
+      id: 11,
+      name: "Guitarra Customizada",
+      image: "guitarra_11",
+      description: "Diseño único y personalizable.",
+      price: 800
+    },
+    {
+      id: 12,
+      name: "Guitarra Barítona",
+      image: "guitarra_12",
+      description: "Sonidos profundos y versátiles.",
+      price: 550
+    }
+  ];
